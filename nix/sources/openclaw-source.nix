6c7d1080f3c006ac56fd594af186f967abe40c59
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-02-06 17:32:58 -0800

    pin: update to jeanlucthumm/openclaw jeanluc-fixes 19e7f2e

--- nix/sources/openclaw-source.nix
+++ nix/sources/openclaw-source.nix
@@ -3,7 +3,7 @@
 {
   owner = "jeanlucthumm";
   repo = "openclaw";
-  rev = "1a02b2ab44772dea88c7f9a83cc3d5d9e5d27f62";
-  hash = "sha256-unhGjn5EFhV3g4rYCw16TGjH66fwwejRJCmumf836x0=";
-  pnpmDepsHash = "sha256-8K4lmbvxe307ceYN7SlzbIJ2p8wFele+v/agyaLI63A=";
+  rev = "19e7f2ef28354a677feba1d44937357b07a72630";
+  hash = "sha256-gVWANMKr4KwO2QsBVE+XJuxamIbyJN5gCKJsnCBI4gI=";
+  pnpmDepsHash = "sha256-sfu8dm1KCGzo/6RcqrvmF1mBB78LcWs1YTcWX5hDuGw=";
 }

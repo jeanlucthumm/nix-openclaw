clawdbot/nix-clawdbot

test(nixos): add VM integration test for system service module
Tests:
- Service starts successfully
- User/group created (clawdbot:clawdbot)
- State directories exist with correct ownership
- Config file symlinked to /var/lib/clawdbot
- Hardening: ProtectHome hides /home from service
- Service runs as clawdbot user

Run with: nix build .#checks.x86_64-linux.nixos-module -L
Interactive: nix build .#checks.x86_64-linux.nixos-module.driverInteractive

flake.nix
@@ -55,6 +55,10 @@
           config-options = pkgs.callPackage ./nix/checks/moltbot-config-options.nix {
             sourceInfo = sourceInfoStable;
           };
+          nixos-module = import ./nix/checks/nixos-module-test.nix {
+            inherit pkgs;
+            clawdbotModule = self.nixosModules.clawdbot;
+          };
         } else {});
 
         devShells.default = pkgs.mkShell {

jeanlucthumm/nix-openclaw

flake.nix
@@ -90,6 +90,10 @@
                   hm-activation = import ./nix/checks/openclaw-hm-activation.nix {
                     inherit pkgs home-manager;
                   };
+                  nixos-module = import ./nix/checks/nixos-module-test.nix {
+                    inherit pkgs;
+                    openclawModule = self.nixosModules.openclaw;
+                  };
                 }
               else
                 { }